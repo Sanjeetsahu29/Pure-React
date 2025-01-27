d3914d6ed3960cfe3418126ee30c656491131d3a
Sanjeet Sahu <sanjeetsahus29@gmail.com>
2025-01-27 12:32:23 +0530

    adding code

---
+++ App.js
@@ -0,0 +1,17 @@
+// const heading  = React.createElement('div', {id:'heading'}, 
+//     React.createElement('h1', null, 'Hello World from Sanjeet kumar Sahu😊'),
+//     React.createElement('p', null, 'This is a simple paragraph written by Sanjeet kumar Sahu in React Js')
+// );
+
+
+const targetElement = React.createElement('div',{id:'parent'},
+    React.createElement('div',{id:'child1'},
+        React.createElement('h1',{},'Hello World from Sanjeet kumar Sahu 🚀'),
+        React.createElement('p',{},'This is a simple paragraph written by Sanjeet kumar Sahu in React Js')
+    ),
+    React.createElement('div',{id:'child2'},
+        React.createElement('p',{id:'info'}, 'I am learning React, wish me Good Luck to become a great React Developer')
+    )
+)
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(targetElement);
