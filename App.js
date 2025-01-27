618d16e52eea8b7fd07bb67e77011dad16d62624
Sanjeet Sahu <sanjeetsahus29@gmail.com>
2025-01-27 12:39:14 +0530

    Adding square bracket to wrap the child element

--- App.js
+++ App.js
@@ -5,9 +5,10 @@
 
 
 const targetElement = React.createElement('div',{id:'parent'},
-    React.createElement('div',{id:'child1'},
+    React.createElement('div',{id:'child1'},[
         React.createElement('h1',{},'Hello World from Sanjeet kumar Sahu 🚀'),
         React.createElement('p',{},'This is a simple paragraph written by Sanjeet kumar Sahu in React Js')
+      ]
     ),
     React.createElement('div',{id:'child2'},
         React.createElement('p',{id:'info'}, 'I am learning React, wish me Good Luck to become a great React Developer')
